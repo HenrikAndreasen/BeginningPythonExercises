ec34ca4365c16fd3d6147259cfe2b9ea655b9561
HenrikAndreasen <henrikandreasen0@gmail.com>
2012-11-30 11:15:23 +0100

    Playing

---
+++ PlayingAround/src/BookExcercises.py
@@ -0,0 +1,26 @@
+'''
+Created on 29/11/2012
+
+@author: Henrik
+'''
+for i in range(10, 0, -1):
+    print("T-minus: ",i)
+
+    
+
+
+
+
+
+'''age=0
+while True:
+how_old=input("Enter your age: ")
+if how_old == "No":
+    print ("Don't be ashamed of your age!")
+    break
+num=int(how_old)
+age=age+num
+print("Your age is :")
+print(age)
+print("That is old!")
+'''
